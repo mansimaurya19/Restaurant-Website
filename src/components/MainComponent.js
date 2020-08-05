77f25356a8b714eab5019cee1373efe08f8a5337
mansimaurya19 <mansimaurya0@gmail.com>
2020-08-05 11:17:52 +0530

    Control Forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import About from './AboutComponent';
 import Contact from './ContactComponent';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
-//import DishDetail from './DishdetailComponent';
+import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
@@ -32,12 +33,22 @@ class Main extends Component {
 				/>
 			);
 		};
+		const DishWithId = ({ match }) => {
+			return (
+				<DishDetail
+					dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+					comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
+				/>
+			);
+		};
 		return (
 			<div>
 				<Header />
 				<Switch>
 					<Route path='/home' component={HomePage} />
 					<Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+					<Route path='/menu/:dishId' component={DishWithId} />
+					<Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
 					<Route exact path='/contactus' component={Contact} />
 					<Redirect to='/home' />
 				</Switch>
